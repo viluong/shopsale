005c2e65c3db2a11dfb1ce496744dfedc622fa41
viluong <viluong1610@gmail.com>
2022-05-01 11:58:51 +0700

    Update readme

--- backend/exception/tests.py
+++ backend/exception/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.exceptions import AuthenticationFailed
+
+from exception.utils import custom_exception_handler
+
+
+class TestException(TestCase):
+    def setUp(self):
+        pass
+
+    def test_custom_exception_handler(self):
+        args = (AuthenticationFailed(), None)
+        self.assertNotEqual(custom_exception_handler(*args), None)
